989f9c47540d0e88ae03b94aec253d7a76644e5a
Luiz Henrique <henrique.luiz-sup@univem.edu.br>
2021-09-16 23:40:23 -0300

    test

--- src/components/layout/SideBarOpenButton.jsx
+++ src/components/layout/SideBarOpenButton.jsx
@@ -3,6 +3,6 @@ import { FaBars } from "react-icons/fa";
 
 import styles from './SideBarOpenButton.module.css'
 
-export default props => 
+export default props =>
     <button className={styles.sidebar_open_btn} onClick={e => {props.onClicar()}}><FaBars /></button>
     
